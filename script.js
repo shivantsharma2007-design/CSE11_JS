752440a39baa320a89f456c991c5f8848a315aa0
Kartik Sharma <shivantsharma2007@gmail.com>
2026-02-24 09:43:15 +0530

    2nd commit

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
-console.log("Hello, World!");
-document.write("Hello, World!");
+var num=10;
+console.log(num);
+console.log(typeof num);
+num = false;
+console.log(num);
+console.log(typeof num);
+num = "kartik sharma ";
+console.log(num);
+console.log(typeof num);
+num = null;
+console.log(num);
+console.log(typeof num);
+num = undefined;
+console.log(num);
+console.log(typeof num);
+var sayHello = function(){
+    alert("Hello World");
+}
+const sym1=Symbol("4");
+console.log(sym1);
+const sym2=Symbol("4");
+console.log(sym2);
+if(sym1===sym2){
+    console.log("true");
+}
+else{    console.log("false");
+}
